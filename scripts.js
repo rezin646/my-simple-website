d01f8cbd8b645dead5749b216459ffb85fdc04de
rezin646 <rezintp10@gmail.com>
2025-02-01 22:05:41 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Example of smooth scroll for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
